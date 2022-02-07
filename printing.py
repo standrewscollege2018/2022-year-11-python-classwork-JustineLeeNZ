ebdebccccb10983747696fc9263c42d8b266f935
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-08 09:48:16 +1300

    print, variables, f-strings

---
+++ printing.py
@@ -0,0 +1,23 @@
+# how to print and use variables
+
+# print statement with 1 parameter
+print("Hello world!")
+
+# print statement with several parameters
+print("I am", 15, "years old")
+
+# declare a variable and set its value
+age = 21
+
+#display current value for age
+print("I am", age, "years old")
+
+# change value of age variable
+age = 100
+#display current value for age
+print("I am", age, "years old")
+
+# change value of age variable
+age = 32
+# use f-string to display current value for age
+print(f"I am {age} years old")
