34596c8a933dbc8cbe78b93edbb4bbc18c98ea8c
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-09 13:58:38 +1300

    newline and input

--- printing.py
+++ printing.py
@@ -21,3 +21,19 @@ print("I am", age, "years old")
 age = 32
 # use f-string to display current value for age
 print(f"I am {age} years old")
+
+#using \n newline
+print("\n347 Papanui Rd,\nChristchurch,\nNZ")
+
+
+# getting string input
+name = input("Enter your name")
+print(f"Hi {name}")
+
+age = int(input("Enter your age"))
+new_age = age*2
+print(f"Twice your age is {new_age}")
+
+# casting to a float
+height = input("Enter your height")
+height = float(height)
