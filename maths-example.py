b22d3ed6263ba14a2edde7c03f26064fdaadbcf3
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-10 22:22:51 +1300

    Maths - basic maths operators

---
+++ maths-example.py
@@ -0,0 +1,27 @@
+# maths in python
+#
+num1 = 15
+num2 = 7
+
+# addition
+
+
+
+#subtraction
+
+
+#multiplication
+
+
+#division
+
+
+#floor division
+
+
+# modulo - division - only remainder
+
+
+# more about modulo
+
+
