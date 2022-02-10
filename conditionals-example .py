a46ce07970f2672d6b25564c267105bb74558760
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-10 22:40:46 +1300

    Conditionals - how to

---
+++ conditionals-example .py
@@ -0,0 +1,15 @@
+# how to code if statement (conditionals)
+
+# check age to see if can drive (if-else)
+age = 16
+
+
+
+
+
+
+
+# if statement with more than 2 outcomes
+# check if can play an age restricted game (if-elif-else)
+
+
