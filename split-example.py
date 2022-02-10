8e69cd4e4596194e98de4f921896eb543e9979a2
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-10 22:22:19 +1300

    Split function
    How to use it

---
+++ split-example.py
@@ -0,0 +1,10 @@
+# how to use split() with strings
+
+# split an existing string into multiple variables
+name = "This is a string"
+
+
+# split user input into multiple variables
+
+
+# split values that are numbers
