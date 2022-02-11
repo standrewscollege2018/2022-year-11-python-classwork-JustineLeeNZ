23d45fd23f1db0e5dbe6c0fd6f37d9928436256d
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-11 15:08:57 +1300

    Maths operators

--- maths-example.py
+++ maths-example.py
@@ -4,24 +4,30 @@ num1 = 15
 num2 = 7
 
 # addition
-
+result = num1 + num2
+print(f"Addition: {result}")
 
 
 #subtraction
-
+result = num1 - num2
+print(f"Subtraction: {result}")
 
 #multiplication
-
+result = num1 * num2
+print(f"Multiplication: {result}")
 
 #division
-
+result = num1 / num2
+print(f"Division: {result}")
 
 #floor division
-
+result = num1 // num2
+print(f"{num2} goes into {num1} {result} times")
 
 # modulo - division - only remainder
+result = num1 % num2
+print(f"Dividing {num1} by {num2} gives the remainder {result}")
 
 
-# more about modulo
 
 
--- split-example.py
+++ split-example.py
@@ -1,10 +1,23 @@
 # how to use split() with strings
 
+#e.g. two inputs()
+name = input("Enter your name")
+print(f"Hello {name}, you are {age} years old")
+
 # split an existing string into multiple variables
-name = "This is a string"
 
 
 # split user input into multiple variables
+name = "This is a string too"
+a,b,c,d, e = name.split(" ")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
 
+
+day, month, year = input("Enter a date").split("/")
+print(f"American date format:{month}-{day}-{year} ")
 
 # split values that are numbers
