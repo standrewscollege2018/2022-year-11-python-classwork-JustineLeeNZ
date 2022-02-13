e6e08da9832baaf380234e227336a5d2d171b372
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-14 10:20:02 +1300

    basic if code added

--- conditionals-example .py
+++ conditionals-example .py
@@ -1,7 +1,17 @@
 # how to code if statement (conditionals)
 
+#constant for age can get licence
+LICENCE_AGE = 16
+
 # check age to see if can drive (if-else)
-age = 16
+age = 10
+
+if age >= LICENCE_AGE:
+    print("You are old enough to sit your licence")
+    print("Congratulations")
+else:
+    print(f"You will be old enough in {16-age} years")
+    print("End of program")
 
 
 
@@ -13,3 +23,6 @@ age = 16
 # check if can play an age restricted game (if-elif-else)
 
 
+
+
+# if statement that checks multiple conditions
