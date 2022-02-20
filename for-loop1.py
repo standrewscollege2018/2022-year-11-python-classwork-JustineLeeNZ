5beda8db22210184365f1fd09008a23d0b03e882
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-21 10:40:56 +1300

    basic for loops with variables

--- for-loop1.py
+++ for-loop1.py
@@ -1,9 +1,13 @@
 # for loops
 
+start_num = 1
+stop_num = 20
+
+for num in range(start_num,stop_num,1):
+    print(num)
+
 start_num = 20
 stop_num = 0
-
-
 # what will this code do?
 for num in range(start_num,stop_num,-1):
     print(num)
