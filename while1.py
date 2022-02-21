52c8cbeafa0f24333f01c29e62674a9fdab09c54
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-22 09:02:59 +1300

    simple while loop

---
+++ while1.py
@@ -0,0 +1,22 @@
+# while loop asks for input and display message until "n" is entered
+
+# Boolean variable to control loop
+keep_asking = True
+
+# while loop runs while keep_asking is True
+while keep_asking == True:
+    # get user's input
+    words = input("Enter some text")
+
+
+    # check if loop should stop
+    if words == "n":
+
+        # set Boolean to False so loop stops
+        keep_asking = False
+
+    # loop should continue
+    else:
+        # print out a message
+        print(words)
+
