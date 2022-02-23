339a197cfb46d4babba4ea8630ee5a0bb6827981
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-23 14:35:41 +1300

    basic guessing game

--- guessing-game.py
+++ guessing-game.py
@@ -5,10 +5,10 @@ keep_asking = True
 # Start a while loop, enabling guesses to be made
 # If the guess is wrong, say "Incorrect", and repeat
 # If the guess is correct, end the loop and say "Correct"
-while ask_keeping == True:
-  guess = int(input(''))
+while keep_asking == True:
+  guess = int(input('Enter a number'))
   if guess == answer:
     print("Correct")
-    ask_keeping = False
+    keep_asking = False
   else:
       print('Incorrect')
