93917a5ad71dda22b52b357028b2b7b0afcb642b
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-26 15:47:23 +1300

    Lists with for loops

---
+++ list-for-loops.py
@@ -0,0 +1,15 @@
+# create a list of cars
+car_list = ["Ford", "Honda", "Tesla", "Ferrari", "Subaru", "Porsche"]
+
+# use temporary variable in for loop to print list
+for car in car_list:
+    print(f"This car is a {car}")
+
+# get length of list
+length = len(car_list)
+print(length)
+
+# use range with for loop to print numbered list
+for i in range(0,len(car_list)):
+    print(car_list[i])
+
