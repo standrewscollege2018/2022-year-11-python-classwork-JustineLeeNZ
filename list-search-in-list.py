8e2718aae54e84b5884f1dfd4b4264842b8af461
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-26 15:47:39 +1300

    List - search for item

---
+++ list-search-in-list.py
@@ -0,0 +1,15 @@
+# create a list of cars
+car_list = ["Ford", "Honda", "Ferrari", "Subaru"]
+
+
+# ask user for brand to search for
+brand = input("Enter a brand: ")
+
+
+# check if brand is in the list
+if brand in car_list:
+    print("Yes")
+else:
+    print("No")
+
+
