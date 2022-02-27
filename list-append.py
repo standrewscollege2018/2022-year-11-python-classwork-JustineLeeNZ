bfc34aad1e627acaa2d9b0153feade16a9b28938
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-28 10:04:12 +1300

    Add to a list

---
+++ list-append.py
@@ -0,0 +1,18 @@
+# add to a list
+
+# create a list of cars
+car_list = ["Ford", "Honda", "Tesla", "Ferrari", "Subaru", "Porsche"]
+
+#DEBUG
+print(car_list)
+
+# get name of new car to add
+new_car = input("Enter a car name")
+
+# add new car to end of list
+car_list.append(new_car)
+
+#DEBUG
+print(car_list)
+
+
