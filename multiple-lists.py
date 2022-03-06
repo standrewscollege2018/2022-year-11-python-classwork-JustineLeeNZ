8c5322c6b9310192ad37bcd807e589fe3c0162f2
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-03-07 09:55:40 +1300

    demo code

---
+++ multiple-lists.py
@@ -0,0 +1,10 @@
+
+
+names = ["John", "Lexi", "Sam"]
+ages = [16, 15, 16]
+eyes = ["Brown", "Blue", "Hazel"]
+
+length = len(names)
+
+for i in range(length):
+    print(f"{names[i]} is {ages[i]} years old and has {eyes[i]} eyes")
