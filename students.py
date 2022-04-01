05fdd5998de3115a586ade23d28d12f5989d77f1
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-04-01 15:14:10 +1300

    student example simple query

---
+++ students.py
@@ -0,0 +1,19 @@
+# import sqlite3 library
+import sqlite3
+
+# define database
+DATABASE = 'student_info.db'
+
+# connect to database
+connection = sqlite3.connect(DATABASE)
+cursor = connection.cursor()
+
+# select student names from student table
+cursor.execute("SELECT student_id, first_name, last_name FROM student")
+results = cursor.fetchall()
+
+for result in results:
+    print(f"{result[0]:5} {result[1]:10} {result[2]:15}")
+
+
+
